36cb704a258346d31b6a425cffcf895d130d6e19
teguh <teguh@kali>
2023-04-18 00:08:38 +0700

    initial commit

---
+++ cmd/random.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Get a random dad joke",
+	Long: `This command fetches a random dad joke from the icanhazdadjoke api`,
+	Run: func(cmd *cobra.Command, args []string) {
+		getRandomJoke()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type Joke struct {
+	ID	string `json:"id"`
+	Joke	string `json:"joke"`
+	Status	string `json:"status"`
+}
+
+func getRandomJoke(){
+	fmt.Println("Get random dad joke :P")
+}
