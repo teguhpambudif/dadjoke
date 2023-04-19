d31d5f43873f97046f28beaa429f95a01be21609
teguh <teguh@kali>
2023-04-19 20:56:42 +0700

    work flawlesly

--- cmd/random.go
+++ cmd/random.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -14,7 +17,7 @@ import (
 var randomCmd = &cobra.Command{
 	Use:   "random",
 	Short: "Get a random dad joke",
-	Long: `This command fetches a random dad joke from the icanhazdadjoke api`,
+	Long:  `This command fetches a random dad joke from the icanhazdadjoke api`,
 	Run: func(cmd *cobra.Command, args []string) {
 		getRandomJoke()
 	},
@@ -35,11 +38,45 @@ func init() {
 }
 
 type Joke struct {
-	ID	string `json:"id"`
-	Joke	string `json:"joke"`
-	Status	string `json:"status"`
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
 }
 
-func getRandomJoke(){
-	fmt.Println("Get random dad joke :P")
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		fmt.Printf("Could not unmarshall responseBytes %v", err)
+	}
+	fmt.Println(string(joke.Joke))
+}
+
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		baseAPI,
+		nil,
+	)
+
+	if err != nil {
+		log.Printf("Could not get random dad joke, %v", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "Dadjoke CLI (learning using local machine)")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Printf("Could not make request. %v", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Could not read response body. %v", err)
+	}
+
+	return responseBytes
 }
